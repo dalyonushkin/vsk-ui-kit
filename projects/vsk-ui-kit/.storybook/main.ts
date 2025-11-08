cca82444cafd0d0eb2cd2efa970f0150fac2590f
Denis Alyonushkin <denis.alyonushkin@gmail.com>
2025-11-08 13:48:06 +0300

    feat: расширена конфигурация storybook

--- projects/vsk-ui-kit/.storybook/main.ts
+++ projects/vsk-ui-kit/.storybook/main.ts
@@ -7,7 +7,9 @@ const config: StorybookConfig = {
   ],
   "addons": [
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
+    "@storybook/addon-designs",
+    "@storybook/addon-a11y"
   ],
   "framework": {
     "name": "@storybook/angular",
--- projects/vsk-ui-kit/.storybook/preview.ts
+++ projects/vsk-ui-kit/.storybook/preview.ts
@@ -1,16 +1,50 @@
 import type { Preview } from '@storybook/angular'
 import { setCompodocJson } from "@storybook/addon-docs/angular";
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS,DEFAULT_VIEWPORT} from 'storybook/viewport';
 import docJson from "../documentation.json";
+
 setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
+    tags: ['autodocs'],
+    a11y: {
+      options: {
+        /*
+         * Opt in to running WCAG 2.x AAA rules
+         * Note that you must explicitly re-specify the defaults (all but the last array entry)
+         * See https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter-examples for more details
+         */
+        runOnly: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice', 'wcag2aaa'],
+      },
+      config: {
+        rules: [
+          { id: 'aria-allowed-attr', enabled: true },
+          { id: 'aria-required-attr', enabled: true },
+          { id: 'aria-required-children', enabled: true },
+          { id: 'aria-required-parent', enabled: true },
+          { id: 'aria-roles', enabled: true },
+          { id: 'aria-valid-attr', enabled: true },
+          { id: 'aria-valid-attr-value', enabled: true },
+          { id: 'aria-prohibited-attr', enabled: true },
+        ],
+      },
+    },
+    viewport: {
+      options: {...MINIMAL_VIEWPORTS,...INITIAL_VIEWPORTS},
+    },
     controls: {
       matchers: {
        color: /(background|color)$/i,
        date: /Date$/i,
       },
     },
+    docs: {
+      codePanel: true,
+    },
+  },
+  initialGlobals: {
+    viewport: { value: 'ipad', isRotated: false },
   },
 };
 
