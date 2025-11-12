8cc704b14c349a7d05f1da03b8781e61c0ea9af2
Denis Alyonushkin <denis.alyonushkin@gmail.com>
2025-11-12 16:02:09 +0300

    настроил генерацию llms.txt при сборке static storybook

---
+++ projects/vsk-ui-kit/.storybook/llms-extractor.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  distPath: 'storybook-static',
+  summaryTitle: 'ВСК UI Kit Components',
+  summaryDescription: 'Компоненты библиотеки VSK UI Kit с описаниями и примерами использования.',
+}
